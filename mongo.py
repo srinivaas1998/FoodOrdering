03b93935fc955a3959e2df7cc839b01dcf116a57
Raghav Srinivaas M <63859534+srinivaas1998@users.noreply.github.com>
2021-05-21 08:06:07 +0530

    Create mongo.py

---
+++ mongo.py
@@ -0,0 +1,45 @@
+import pymongo
+
+
+def insert_data(name,phone,password,latitude,longitude):
+    myClient = pymongo.MongoClient()
+    db = myClient["rest"]
+    col= db["Customer"]
+    rec1 = {
+        "name":name,
+        "phone":phone,
+        "pass":password,
+        "location_customer":{'type':"Point", "coordinates": [latitude,longitude]},
+        "location_rest": {'type': "Point", "coordinates": ['13.0615','80.2615']}
+
+    }
+    rec_id = col.insert_one(rec1)
+    print("Data inserted with record id ",rec_id)
+    cursor = col.find()
+    for x in cursor:
+        print(x)
+
+
+#insert_data("MGS","9443484505","chinku1998","13.0888","80.2620")
+
+
+def select_data(phone,password):
+
+    myClient = pymongo.MongoClient()
+    db = myClient["rest"]
+    col = db["Customer"]
+    rec1 = {
+        "phone": phone,
+        "pass": password
+    }
+    ph=None
+    pa=None
+    for doc in col.find(rec1):
+        print(doc)
+        ph=doc["phone"]
+
+        pa=doc["pass"]
+    if((phone==ph) and (password==pa)):
+        return 1
+
+#print(select_data("123456789","chinku1998"))
